d3f1bf466bff3219e8b8595a1ab34a30cf292c03
Flyerme <gao.eison@gmail.com>
2017-08-08 22:05:09 -0400

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,7 +1,4 @@
-
-$DB = Database::getInstance();
-$sql="ALTER TABLE `{db_prefix}user`  ADD `getpasstime` INT(10) NOT NULL  AFTER`website`";
-$DB -> query($sql);
+ALTER TABLE `{db_prefix}user`  ADD `getpasstime` INT(10) NOT NULL  AFTER`website`;
 
 
 
