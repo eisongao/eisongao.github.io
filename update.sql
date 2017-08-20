3d00837ad795b0457d2894d565d9090af6c7fd8f
Flyerme <gao.eison@gmail.com>
2017-08-19 23:24:13 -0400

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,4 +1,3 @@
-ALTER TABLE `{db_prefix}user`  ADD `getpasstime` INT(10) NOT NULL  AFTER`website`;
 INSERT INTO `{db_prefix}options` (option_name, option_value) VALUES ('full_screen','y');
 INSERT INTO `{db_prefix}options` (option_name, option_value) VALUES ('preloader','y');
 
