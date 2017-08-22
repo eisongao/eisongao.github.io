27889ac27162e2fdaae989d42fea684a358aaec9
Flyerme <gao.eison@gmail.com>
2017-08-22 13:47:35 -0400

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,6 +1,9 @@
 INSERT INTO `{db_prefix}options` (option_name, option_value) VALUES ('full_screen','y');
 INSERT INTO `{db_prefix}options` (option_name, option_value) VALUES ('preloader','y');
 INSERT INTO `{db_prefix}options` (option_name, option_value) VALUES ('filter_xss','y');
+INSERT INTO `{db_prefix}options` (option_name, option_value) VALUES ('responsive','y');
+INSERT INTO `{db_prefix}options` (option_name, option_value) VALUES ('scrollable','y');
+
 
 
 
