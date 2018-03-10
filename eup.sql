05b62d6de09db73395aba79f29a3552a8dd00682
Flyerme <gao.eison@gmail.com>
2018-03-09 21:24:53 -0500

    Update eup.sql

--- eup.sql
+++ eup.sql
@@ -1,13 +1,4 @@
-DROP TABLE IF EXISTS {$db_prefix}block;
-CREATE TABLE {$db_prefix}block (
-  id int(10) unsigned NOT NULL auto_increment,
-  date int(10) NOT NULL default '0',
- serverip varchar(200) NOT NULL default '',
- attack_num int(10) NOT NULL default '0',
-UNIQUE KEY serverip (serverip),
-KEY block (id)
-)".$table_charset_sql."
-
+ALTER TABLE {$db_prefix}block ADD COLUMN attack_num int(10) NOT NULL default '0' AFTER serverip;
 
 DELETE FROM {$db_prefix}options WHERE option_name= 'webscan_seconds';
 DELETE FROM {$db_prefix}options WHERE option_name= 'webscan_refresh';
